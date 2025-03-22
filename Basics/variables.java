ff1012eba0f018ff773db8fff4063b9a5ce3dfc7
Garvit Rakhecha <garvitrakhecha786@gmail.com>
2025-03-22 12:35:48 +0530

    declaration initialization and updation in variables

---
+++ Basics/variables.java
@@ -0,0 +1,15 @@
+public class variables {
+    public static void main(String[] args) {
+        int x; //declaration
+        x = 20; //initialization
+
+        int y = 25;
+
+        System.out.println(x);
+        System.out.println(y);
+
+        x = x+20; //updations
+        System.out.println(x);
+
+    }
+}
