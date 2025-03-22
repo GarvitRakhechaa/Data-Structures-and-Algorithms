75c9453d1d0eecb10cbb28d1e2aef3da6406ce14
Garvit Rakhecha <garvitrakhecha786@gmail.com>
2025-03-22 13:41:13 +0530

    simple interest program

---
+++ Basics/SimpleInterest.java
@@ -0,0 +1,10 @@
+public class SimpleInterest {
+    public static void main(String[] args) {
+        System.out.println("simple interest");
+        double p = 2000;
+        double r = 3.75;
+        double t = 3.5;
+
+        System.out.println("simple interest is = "+ (p*r*t)/100);
+    }
+}
