4ee90d697735ff24dd673b061c15b6c42ac5f7c2
Garvit Rakhecha <garvitrakhecha786@gmail.com>
2025-03-22 12:45:39 +0530

    Arthmatic opeataion done

---
+++ Basics/ArthmaticOperations.java
@@ -0,0 +1,12 @@
+public interface ArthmaticOperations {
+    public static void main(String[] args) {
+        double y = 5.0;
+        int x = 15;
+
+        System.out.println(x + y);
+        System.out.println(x - y);
+        System.out.println(x * y);
+        System.out.println(x / y);
+        System.out.println(x % y);
+    }
+}
---
+++ Basics/tempCodeRunnerFile.java
@@ -0,0 +1,5 @@
+        System.out.println(x + y);
+        System.out.println(x - y);
+        System.out.println(x * y);
+        System.out.println(x / y);
+        System.out.println(x % y);
